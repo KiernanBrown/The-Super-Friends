ac511fa61e68635a57db45a16c8d4d6b735632ed
Kiernan Brown <kwb6721@g.rit.edu>
2016-12-16 00:34:42 -0500

    Updated to the Final Deliverable. Tweaked the lighting of the scene, updated the textures on some models, and added scripts to allow the user to interact with some models.

---
+++ theSuperFriends_FPFD/Assets/Scripts/CloseAnimate.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloseAnimate : MonoBehaviour
+{
+
+    public float interactDistance = 30.0f;
+    public float delay = 0.0f;
+    public Animator anim;
+    public GameObject player;
+    Vector3 position;
+    float animationTimer = 0.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        anim.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        position = new Vector3(gameObject.transform.position.x, player.transform.position.y, gameObject.transform.position.z);
+        if (animationTimer > 0.0f)
+        {
+            animationTimer -= Time.deltaTime;
+        }
+
+        if (Vector3.Distance(player.transform.position, position) < interactDistance && animationTimer <= 0.0f)
+        {
+            anim.enabled = true;
+            anim.Play(0);
+            animationTimer = anim.GetCurrentAnimatorStateInfo(0).length / anim.speed;
+            animationTimer += delay;
+        }
+    }
+}
---
+++ theSuperFriends_FPFD/Assets/Scripts/Interact.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Interact : MonoBehaviour
+{
+
+    public float interactDistance = 30.0f;
+    public Animator anim;
+    public GameObject player;
+    Vector3 position;
+    float animationTimer = 0.0f;
+
+    // Use this for initialization
+    void Start ()
+    {
+        anim.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        position = new Vector3(gameObject.transform.position.x, player.transform.position.y, gameObject.transform.position.z);
+        if (animationTimer > 0.0f)
+        {
+            animationTimer -= Time.deltaTime;
+        }
+
+        if (Input.GetKeyDown(KeyCode.E) && Vector3.Distance(player.transform.position, position) < interactDistance && animationTimer <= 0.0f)
+        {
+            anim.enabled = true;
+            anim.Play(0);
+            animationTimer = anim.GetCurrentAnimatorStateInfo(0).length / anim.speed;
+        }
+	}
+
+    void OnGUI()
+    {
+        if(Vector3.Distance(player.transform.position, position) < interactDistance && animationTimer <= 0.0f)
+        {
+            GUI.Box(new Rect(Screen.width / 2 - 200, 10, 400, 40), "Press E to Interact with this object!");
+        }
+    }
+}
